5ff58c55fdcb70317e6ff3dceeb10308dcfec595
BalashovaMa <balashova.m.nd@gmail.com>
2023-07-06 15:04:49 +0300

    refactor: weather.js

--- services/storage.service.js
+++ services/storage.service.js
@@ -2,7 +2,7 @@ import { homedir } from 'os';
 import { join } from 'path';
 import { promises } from 'fs';
 
-const filePath = join(homedir(), 'weather_data.json');
+const filePath = join(homedir(), 'weather-data.json');
 
 const TOKEN_DICTIONARY = {
     token: 'token',
@@ -13,7 +13,7 @@ const saveKeyValue = async (key, value) => {
     let data = {};
     if (await isExist(filePath)) {
         const file = await promises.readFile(filePath);
-        data = JSON.parse(file)
+        data = JSON.parse(file);
     }
     data[key] = value;
     await promises.writeFile(filePath, JSON.stringify(data));
@@ -26,15 +26,15 @@ const getKeyValue = async (key) => {
         return data[key];
     }
     return undefined;
-}
+};
 
 const isExist = async (path) => {
     try {
         await promises.stat(path);
         return true;
-    } catch (error) {
+    } catch (e) {
         return false;
     }
-}
+};
 
 export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
